f4776b32414be6a93292a7e057fb467b5e3d033f
trixvenom12 <adityar150805@gmail.com>
2026-06-01 18:32:02 +0530

    fix: cast lazy player module to satisfy dynamic import ts properties

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 import { useCms } from '@/context/CmsContext';
 import { useSearchParams } from 'react-router-dom';
 
-const Player = lazy(() => import('react-player'));
+const Player = lazy(() => import('react-player')) as any;
 
 const PLAYER_CONFIG = {
   file: {
